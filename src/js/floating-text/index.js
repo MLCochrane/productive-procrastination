eef63d2a5b99e455b17e915c8dcee1d20b8091b6
Luke <lukecochrane2@gmail.com>
2019-05-29 15:57:34 -0400

    Moving over 3D floating text sketch

---
+++ src/js/floating-text/index.js
@@ -0,0 +1,93 @@
+import * as THREE from 'three';
+
+export default class FloatingText {
+	constructor() {
+		this.renderer;
+		this.scene;
+		this.camera;
+		this.mesh;
+		this.xVal = 0;
+		this.yVal = 0;
+		this.curX = window.innerWidth;
+		this.curY = window.innerHeight;
+		this.canvas = document.getElementById('FloatingText');
+		this.dims = this.canvas.getBoundingClientRect();
+		this.bufferFile = './src/assets/floating-text.json';
+
+		this.animate = this.animate.bind(this);
+		this.setup = this.setup.bind(this);
+
+		this.bindEvents();
+		this.init();
+	}
+
+	bindEvents() {
+		window.addEventListener('resize', () => {
+			this.onWindowResize();
+		});
+
+		window.addEventListener('mousemove', e => {
+			this.xVal = (e.clientX / window.innerWidth) - 0.5;
+			this.yVal = (e.clientY / window.innerHeight) - 0.5;
+		});
+		window.addEventListener('click', () => {
+			this.renderer.setPixelRatio(window.devicePixelRatio);
+		});
+	}
+
+	init() {
+		this.initScene();
+		this.initCamera();
+		this.initText(this.bufferFile);
+	}
+	initScene() {
+		this.scene = new THREE.Scene();
+	}
+	initCamera() {
+		const distance = 3.5;
+		// this.camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.2, 10 );
+		const aspect = (window.innerWidth / 2) / (window.innerWidth / 4)
+		this.camera = new THREE.OrthographicCamera(distance * aspect / - 2, distance * aspect / 2, distance / 2, distance / - 2, 0.8, 20);
+		this.camera.position.z = 8;
+		this.scene.add(this.camera);
+	}
+	initText(bufferFile) {
+		var loader = new THREE.BufferGeometryLoader();
+		loader.load(bufferFile, geo => {
+			// Add the loaded object to the scene
+			const mat1 = new THREE.MeshBasicMaterial({ color: 0xffffff });
+			const mat2 = new THREE.MeshBasicMaterial({ color: 0x000000 });
+			// Materials passed in to group materialIndex
+			const object = new THREE.Mesh(geo, [mat1, mat2]);
+			this.mesh = object;
+			this.scene.add(object);
+			this.setup(object);
+		}, xhr => {
+			console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+		}, err => {
+			console.error(err);
+		}
+		);
+	}
+	onWindowResize() {
+		this.camera.aspect = (window.innerWidth / 2) / (window.innerWidth / 4);
+		this.camera.updateProjectionMatrix();
+		this.renderer.setSize(window.innerWidth / 2, window.innerWidth / 4);
+	}
+	setup() {
+		this.renderer = new THREE.WebGLRenderer({ antialias: true, canvas: this.canvas, alpha: true });
+		this.renderer.setPixelRatio(window.devicePixelRatio);
+		this.renderer.setSize(this.dims.width, this.dims.height);
+		this.animate();
+	}
+	animate() {
+		requestAnimationFrame(this.animate);
+
+		this.mesh.rotation.x = (this.yVal);
+		this.mesh.rotation.y = (this.xVal);
+		this.render();
+	}
+	render() {
+		this.renderer.render(this.scene, this.camera);
+	}
+}
