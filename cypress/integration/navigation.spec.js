9f102b46c84d1714516ab06f81b3b89d56f9a4db
Luke <lukecochrane2@gmail.com>
2019-11-10 20:40:59 -0500

    Removing commented code

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -110,44 +110,4 @@ describe('Navigation', () => {
     cy.get('.header__list-item a').contains('Light Glow').click();
     cy.get(`[data-barba-namespace="cutout-slider"]`, {timeout: 20000}).should('not.exist');
   });
-
-
-
-  // it('successfully navigates to each sketch and removes previous DOM', () => {
-  //   cy.window().then((win) => {
-  //     cy.spy(win.console, "log")
-  //     cy.spy(win.console, "warn")
-  //     cy.spy(win.console, "error")
-  //   })
-  //   cy.get('#ToggleNav').should('be.visible').click();
-  //   cy.wait(300);
-  //   cy.get('.header__list-item a').should('have.length', 7)
-  //   .each(($li, index, $list) => {
-  //     const link = $li.attr('href');
-  //     const namespace = link.split('/')[1];
-  //     let previous;
-
-  //     cy.get('[data-barba="container"]').then($el => {
-  //       // Checks not only that navigation works but barba correctly removing items from DOM
-  //       previous = $el.attr('data-barba-namespace');
-
-  //       cy.get(`[data-barba-namespace="${namespace}"]`).should('not.exist');
-  //       cy.get($li).click();
-  //       cy.get(`[data-barba-namespace="${namespace}"]`).should('exist');
-  //       cy.url().should('eq', `http://localhost:8080${link}`);
-  //       cy.get(`[data-barba-namespace="${previous}"]`, {timeout: 30000}).should('not.exist');
-  //       cy.get('#ToggleNav').should('be.visible').click();
-  //       cy.wait(300);
-
-  //       // Returns home
-  //       if (index === $list.length - 1) {
-  //         cy.get(`[data-barba-namespace="home"]`).should('not.exist');
-  //         cy.get('.brand__logo a').click();
-  //         cy.get('[data-barba-namespace="home"]').should('exist');
-  //         cy.url().should('eq', 'http://localhost:8080/');
-  //         cy.get(`[data-barba-namespace="${previous}"]`).should('not.exist');
-  //       }
-  //     })
-  //   })
-  // });
 });
