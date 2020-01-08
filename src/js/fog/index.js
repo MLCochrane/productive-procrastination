89200b336dd017f481e0eed6982c9ed07a53eceb
Luke <lukecochrane2@gmail.com>
2020-01-08 15:27:42 -0500

    Cleaning up class a bit, named imports, etc

--- src/js/fog/index.js
+++ src/js/fog/index.js
@@ -1,9 +1,27 @@
-import * as THREE from 'three';
+import {
+  Scene,
+  WebGLRenderTarget,
+  WebGLRenderer,
+  RGBFormat,
+  NearestFilter,
+  DepthTexture,
+  UnsignedShortType,
+  PerspectiveCamera,
+  OrthographicCamera,
+  PointLight,
+  AmbientLight,
+  TorusKnotBufferGeometry,
+  PlaneBufferGeometry,
+  MeshLambertMaterial,
+  ShaderMaterial,
+  Mesh,
+  Vector3,
+} from 'three';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 /*
- *  Class for loading and rendering 3D text
+ *  Class for threejs fog scene
  */
 
 export default class Fog {
@@ -21,7 +39,7 @@ export default class Fog {
     this.animate = this.animate.bind(this);
     this.setup = this.setup.bind(this);
     this.handleResize = this.handleResize.bind(this);
-    this.setupPost = this.setupPost.bind(this);
+    this.initFog = this.initFog.bind(this);
 
     this.bindEvents();
     this.init();
@@ -39,7 +57,7 @@ export default class Fog {
   /**
    *
    * @function destroy
-   * @memberof .prototype
+   * @memberof Fog.prototype
    */
   destroy() {
     window.removeEventListener('resize', this.handleResize);
@@ -75,7 +93,7 @@ export default class Fog {
     this.initScene();
     this.initCamera();
     this.initLights();
-    this.setupPost();
+    this.initFog();
     this.initGeo();
   }
 
@@ -85,17 +103,17 @@ export default class Fog {
    * @memberof Fog.prototype
    */
   initScene() {
-    this.scene = new THREE.Scene();
+    this.scene = new Scene();
 
-    this.target = new THREE.WebGLRenderTarget(window.innerWidth, window.innerHeight);
-    this.target.texture.format = THREE.RGBFormat;
-    this.target.texture.minFilter = THREE.NearestFilter;
-    this.target.texture.magFilter = THREE.NearestFilter;
+    this.target = new WebGLRenderTarget(window.innerWidth, window.innerHeight);
+    this.target.texture.format = RGBFormat;
+    this.target.texture.minFilter = NearestFilter;
+    this.target.texture.magFilter = NearestFilter;
     this.target.texture.generateMipmaps = false;
     this.target.stencilBuffer = false;
     this.target.depthBuffer = true;
-    this.target.depthTexture = new THREE.DepthTexture();
-    this.target.depthTexture.type = THREE.UnsignedShortType;
+    this.target.depthTexture = new DepthTexture();
+    this.target.depthTexture.type = UnsignedShortType;
   }
 
   /**
@@ -104,7 +122,7 @@ export default class Fog {
    * @memberof Fog.prototype
    */
   initCamera() {
-    this.camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 70);
+    this.camera = new PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 70);
 
     this.camera.position.z = 30;
     this.scene.add(this.camera);
@@ -116,8 +134,8 @@ export default class Fog {
    * @memberof Fog.prototype
    */
   initGeo() {
-    const geo = new THREE.TorusKnotBufferGeometry(1, 0.3, 128, 64);
-    const mat = new THREE.MeshLambertMaterial({
+    const geo = new TorusKnotBufferGeometry(1, 0.3, 128, 64);
+    const mat = new MeshLambertMaterial({
       color: 0x0000ff
     });
 
@@ -128,7 +146,7 @@ export default class Fog {
       const r = Math.random() * 2.0 * Math.PI;
       const z = (Math.random() * 2.0) - 1.0;
       const zScale = Math.sqrt(1.0 - z * z) * scale;
-      const mesh = new THREE.Mesh(geo, mat);
+      const mesh = new Mesh(geo, mat);
       mesh.position.set(
         Math.cos(r) * zScale,
         Math.sin(r) * zScale,
@@ -141,9 +159,14 @@ export default class Fog {
     this.setup();
   }
 
+  /**
+   * Adds lights to the scene
+   * @function initLights
+   * @memberof Fog.prototype
+   */
   initLights() {
-    this.light = new THREE.PointLight(0xf57d7d, .6, 20, .5);
-    this.light2 = new THREE.PointLight(0xf57d7d, .4, 20, .5);
+    this.light = new PointLight(0xf57d7d, .6, 20, .5);
+    this.light2 = new PointLight(0xf57d7d, .4, 20, .5);
 
     this.light.position.set(2, 0, -5);
     this.light2.position.set(-0.5, 3, 5);
@@ -154,40 +177,61 @@ export default class Fog {
     this.scene.add(this.light);
     this.scene.add(this.light2);
 
-    this.light3 = new THREE.AmbientLight(0xffffff, 0.5);
+    this.light3 = new AmbientLight(0xffffff, 0.5);
     this.scene.add(this.light3);
   }
 
-  setupPost() {
+  /**
+   * Sets up fog post processing scene
+   * @function initFog
+   * @memberof Fog.prototype
+   */
+  initFog() {
     const {
       camera,
       target
     } = this;
+
+    const uniforms = {
+      cameraNear: {
+          value: camera.near
+        },
+      cameraFar: {
+        value: camera.far
+      },
+      tDiffuse: {
+        value: target.texture
+      },
+      tDepth: {
+        value: target.depthTexture
+      },
+      intensity: {
+        value: 2.0,
+      },
+      fogColor: {
+        value: new Vector3(0.3, 0.9, 0.3),
+      }
+    }
     // Setup post processing stage
-    this.postCamera = new THREE.OrthographicCamera( - 1, 1, 1, - 1, 0, 1 );
-    var postMaterial = new THREE.ShaderMaterial( {
+    this.postCamera = new OrthographicCamera( - 1, 1, 1, - 1, 0, 1 );
+    const postMaterial = new ShaderMaterial( {
       vertexShader: document.getElementById('vertexshader').textContent,
       fragmentShader: document.getElementById('fragmentshader').textContent,
-      uniforms: {
-        cameraNear: { value: camera.near },
-        cameraFar: { value: camera.far },
-        tDiffuse: { value: target.texture },
-        tDepth: { value: target.depthTexture }
-      }
+      uniforms,
     });
-    var postPlane = new THREE.PlaneBufferGeometry( 2, 2 );
-    var postQuad = new THREE.Mesh( postPlane, postMaterial );
-    this.postScene = new THREE.Scene();
+    const postPlane = new PlaneBufferGeometry( 2, 2 );
+    const postQuad = new Mesh( postPlane, postMaterial );
+    this.postScene = new Scene();
     this.postScene.add( postQuad );
   }
 
   /**
-   *
+   * Sets up renderer, controls, and starts animation loop
    * @function setup
    * @memberof Fog.prototype
    */
   setup() {
-    this.renderer = new THREE.WebGLRenderer({
+    this.renderer = new WebGLRenderer({
       antialias: true,
       canvas: this.canvas,
       alpha: true
