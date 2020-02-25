3027e75ee6c01da028893c9643af9683192b1d85
Luke <lukecochrane2@gmail.com>
2020-02-25 18:23:07 -0500

    Updating overlay to use options object, expanding testing

--- src/js/utils/overlay-helper/helper.js
+++ src/js/utils/overlay-helper/helper.js
@@ -1,29 +1,85 @@
 /**
-   * 
-   * @param {Number} input - Number representing the type of input to display.
-   * @param {String} message - Message to display to user.
-   * @param {Element} parent - HTML element to insert overlay onto
-   * @param {Number} timeout - Timeout in milliseconds before automatically hiding overlay. 
-   * @param {String} color - String of Hex color to use for background.
-   * @param {Boolean} button - Whether to display a "close" button.
-   */
-function overlay(input, message, parent, timeout, color, button) {
-  if (input === undefined) throw 'Must supply input value to indicate type of input'
-  if (typeof input !== 'number') throw 'Input argument must be a number'
-  if (input < 1 || input > 5) throw 'Input argument must be a positive value from 1 to 5'
-
-  if (message && typeof message !== 'string') throw 'Message argument must be a string'
-  if (message && message.length > 200) throw 'Message string cannot be longer than 200 characters.'
-
-  if (!isElement(parent)) throw 'Parent argument must be a valid element'
-
-  if (timeout && typeof timeout !== 'number') throw 'Timeout argument must be a positive number'
-}
-
+ * Checks if passed argument is an element of not
+ * @function isElement
+ * @param {any} element - Argument to check if an element.
+ * @returns {Boolean}
+ */
 function isElement(element) {
-  return element instanceof Element || element instanceof HTMLDocument;  
+  return element instanceof Element || element instanceof HTMLDocument;
 }
 
-export {
-  overlay
-};
+/**
+ * Checks inputs and throws relevant error if necessary.
+ * @function checkInputs
+ */
+function checkInputs({
+  input,
+  message,
+  parent,
+  timeout,
+  color,
+  button,
+}) {
+  let msg = '';
+  const colorReg = /^(#[0-9a-f]{3}|#(?:[0-9a-f]{2}){2,4}|(rgb|hsl)a?\((-?\d+%?[,\s]+){2,3}\s*[\d.]+%?\))$/;
+  if (input && typeof input !== 'number') msg = 'Input argument must be a number';
+  else if (input < 1 || input > 5) msg = 'Input argument must be a positive value from 1 to 5';
+  else if (message && typeof message !== 'string') msg = 'Message argument must be a string';
+  else if (message && message.length > 200) msg = 'Message string cannot be longer than 200 characters.';
+  else if (!isElement(parent)) msg = 'Parent argument must be a valid element';
+  else if (timeout && typeof timeout !== 'number') msg = 'Timeout argument must be a positive number';
+  else if (color != null && !colorReg.test(color)) msg = 'Color argument must be a valid CSS color';
+  else if (button != null && typeof button !== 'boolean') msg = 'Button argument must be a boolean';
+
+  return msg === '' ? { error: 0, msg: '' } : { error: 1, msg };
+}
+
+/**
+   * Creates input instruction overlay
+   * @function overlay
+   * @param {Object} options - Options object for specifying params.
+   * @param {Number} options.input - Number representing the type of input to display.
+   * @param {String} options.message - Message to display to user.
+   * @param {Element} options.parent - HTML element to insert overlay onto
+   * @param {Number} options.timeout - Timeout in milliseconds before automatically hiding overlay.
+   * @param {String} options.color - String of Hex color to use for background.
+   * @param {Boolean} options.button - Whether to display a "close" button.
+   */
+export default function overlay(options) {
+  if (options != null) {
+    const { error, msg } = checkInputs(options); // eslint-disable-line prefer-rest-params
+    if (error) throw new Error(msg);
+  }
+
+  const config = {
+    input: options.input || 1,
+    message: options.message || '',
+    parent: options.parent || document.body,
+    timeout: options.timeout || 3000,
+    color: options.color || 'rgba(57,70,70,0.8)',
+    button: options.button || false,
+  };
+
+  let inputType = '';
+  switch (config.input) {
+    case 1:
+      inputType = 'mousemove';
+      break;
+    case 2:
+      inputType = 'drag';
+      break;
+    case 3:
+      inputType = 'scroll';
+      break;
+    case 4:
+      inputType = 'click';
+      break;
+    default:
+      break;
+  }
+
+  const element = document.createElement('div');
+  element.classList.add('input-overlay');
+  element.setAttribute('data-input-type', inputType);
+  config.parent.appendChild(element);
+}
--- src/js/utils/overlay-helper/helper.spec.js
+++ src/js/utils/overlay-helper/helper.spec.js
@@ -1,69 +1,223 @@
-import { overlay } from './helper';
-describe('Instruction overlay', () => {
-  document.body.innerHTML =
-  `<div>
-    <span id="username" />
-  </div>`;
-  const el = document.getElementById('username');
+/* eslint-disable no-undef */
+import {
+  toContainHTML,
+} from '@testing-library/jest-dom/matchers';
 
+import overlay from './helper';
+
+expect.extend({
+  toContainHTML,
+});
+
+describe('Instruction overlay inputs', () => {
+  let el;
+  beforeEach(() => {
+    document.body.innerHTML = `
+    <div>
+    <div id="parent"></div>
+    </div>`;
+    el = document.getElementById('parent');
+  });
 
   test('Handles incorrect input argument', () => {
     expect(() => {
-      overlay();
-    }).toThrow('Must supply input value to indicate type of input');
-
-    expect(() => {
-      overlay('hi');
+      overlay({
+        input: 'hi',
+      });
     }).toThrow('Input argument must be a number');
 
     expect(() => {
-      overlay(2);
+      overlay({
+        input: 2,
+      });
     }).not.toThrow('Input argument must be a number');
-    
+
     expect(() => {
-      overlay(-120);
+      overlay({
+        input: -120,
+      });
     }).toThrow('Input argument must be a positive value from 1 to 5');
 
     expect(() => {
-      overlay(6);
+      overlay({
+        input: 6,
+      });
     }).toThrow('Input argument must be a positive value from 1 to 5');
 
     expect(() => {
-      overlay(1);
+      overlay({
+        input: 1,
+      });
     }).not.toThrow('Input argument must be a positive value from 1 to 5');
   });
 
   test('Handles incorrect message argument', () => {
     expect(() => {
-      overlay(2, 2);
+      overlay({
+        message: 2,
+      });
     }).toThrow('Message argument must be a string');
 
     expect(() => {
-      overlay(2, '000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001');
+      overlay({
+        message: '000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001',
+      });
     }).toThrow('Message string cannot be longer than 200 characters');
 
     expect(() => {
-      overlay(2, 'Click and drag');
+      overlay({
+        message: 'Click and drag',
+      });
     }).not.toThrow('Message string cannot be longer than 200 characters');
-  })
+  });
 
   test('Handles incorrect parent argument', () => {
     expect(() => {
-      overlay(2, 'Hello', '');
+      overlay({
+        parent: '',
+      });
     }).toThrow('Parent argument must be a valid element');
 
     expect(() => {
-      overlay(2, 'Hello', el);
+      overlay({
+        parent: el,
+      });
     }).not.toThrow('Parent argument must be a valid element');
-  })
+  });
 
   test('Handles incorrect timeout argument', () => {
     expect(() => {
-      overlay(2, 'Hello', el, 'jakjfslkj');
+      overlay({
+        parent: el,
+        timeout: 'jakjfslkj',
+      });
     }).toThrow('Timeout argument must be a positive number');
 
     expect(() => {
-      overlay(2, 'Hello', el, '');
+      overlay({
+        parent: el,
+        timeout: [],
+      });
     }).toThrow('Timeout argument must be a positive number');
-  })
+
+    expect(() => {
+      overlay({
+        parent: el,
+        timeout: 1000,
+      });
+    }).not.toThrow('Timeout argument must be a positive number');
+  });
+
+  test('Handles inncorrect color values', () => {
+    const options = {
+      parent: el,
+    };
+
+    expect(() => {
+      options.color = '';
+      overlay(options);
+    }).toThrow('Color argument must be a valid CSS color');
+
+    expect(() => {
+      options.color = 'rgb(1)';
+      overlay(options);
+    }).toThrow('Color argument must be a valid CSS color');
+
+    expect(() => {
+      options.color = '#22';
+      overlay(options);
+    }).toThrow('Color argument must be a valid CSS color');
+
+    expect(() => {
+      options.color = '#ffzf01';
+      overlay(options);
+    }).toThrow('Color argument must be a valid CSS color');
+
+    expect(() => {
+      options.color = '#ff00ff';
+      overlay(options);
+    }).not.toThrow('Color argument must be a valid CSS color');
+
+    expect(() => {
+      options.color = '#eee';
+      overlay(options);
+    }).not.toThrow('Color argument must be a valid CSS color');
+
+    expect(() => {
+      options.color = 'rgb(0,0,0)';
+      overlay(options);
+    }).not.toThrow('Color argument must be a valid CSS color');
+
+    expect(() => {
+      options.color = 'rgba(0, 0, 0, 1)';
+      overlay(options);
+    }).not.toThrow('Color argument must be a valid CSS color');
+  });
+
+  test('Handles inncorrect button values', () => {
+    const options = {
+      parent: el,
+    };
+    expect(() => {
+      options.button = 'f';
+      overlay(options);
+    }).toThrow('Button argument must be a boolean');
+
+    expect(() => {
+      options.button = 1;
+      overlay(options);
+    }).toThrow('Button argument must be a boolean');
+
+    expect(() => {
+      options.button = true;
+      overlay(options);
+    }).not.toThrow('Button argument must be a boolean');
+
+    expect(() => {
+      options.button = false;
+      overlay(options);
+    }).not.toThrow('Button argument must be a boolean');
+  });
+});
+
+describe('Intruction overlay DOM modification', () => {
+  let el;
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <div>
+        <div id="parent"></div>
+      </div>`;
+    el = document.getElementById('parent');
+  });
+
+  test('Adds new element to parent element', () => {
+    const options = {
+      input: 2,
+      parent: el,
+    };
+    overlay(options);
+    expect(el.innerHTML).toEqual(
+      '<div class="input-overlay" data-input-type="drag"></div>',
+    );
+  });
+
+  test('Sets default input type data attribute', () => {
+    overlay({
+      parent: el,
+    });
+    expect(el.innerHTML).toEqual(
+      '<div class="input-overlay" data-input-type="mousemove"></div>',
+    );
+  });
+
+  test('Sets scroll input type data attribute', () => {
+    const options = {
+      input: 3,
+      parent: el,
+    };
+    overlay(options);
+    expect(el.innerHTML).toEqual(
+      '<div class="input-overlay" data-input-type="scroll"></div>',
+    );
+  });
 });
