ed94d6f506eede081ffcbc18f2b237ef70cf8477
Luke <lukecochrane2@gmail.com>
2020-05-11 11:36:30 -0400

    Scroll loop types refactor

--- src/js/scroll-loop/index.ts
+++ src/js/scroll-loop/index.ts
@@ -1,20 +1,29 @@
-import { TimelineMax, Power0 } from 'gsap';
+import { gsap, CSSPlugin } from 'gsap';
 
-/**
- * @typedef {object} WheelEvent
- */
+gsap.registerPlugin(CSSPlugin);
 
 /*
  *  Class for scroll loop sketch
  */
 export default class ScrollingProjects {
+  currentOffset: number;
+  originOffset: number;
+  container: HTMLElement;
+  current: CSSStyleDeclaration;
+  maxHeight: any;
+  tl: GSAPTimeline | null;
+  loopSeconds: number;
+  loopLength: number;
+  start: null;
+  loopTimeout: number;
+  resizeTimeout: number;
   /**
    * Initlaizes variables for class instance.
    */
   constructor() {
-    this.currentOffset = '';
-    this.originOffset = '';
-    this.container = document.querySelector('.thing-holder');
+    this.currentOffset = 0;
+    this.originOffset = 0;
+    this.container = document.querySelector('.thing-holder') as HTMLElement;
     this.current = window.getComputedStyle(this.container);
 
     this.maxHeight = (this.current.getPropertyValue('height').split('px'))[0];
@@ -24,9 +33,10 @@ export default class ScrollingProjects {
     this.loopLength = (this.loopSeconds * 1920) / window.innerWidth;
 
     this.start = null;
-    this.loopTimeout = null;
-    this.resizeTimeout = null;
+    this.loopTimeout = 0;
+    this.resizeTimeout = 0;
 
+    this.initSliding = this.initSliding.bind(this);
     this.handleWheel = this.handleWheel.bind(this);
     this.handleResize = this.handleResize.bind(this);
     this.destroy = this.destroy.bind(this);
@@ -55,9 +65,11 @@ export default class ScrollingProjects {
   destroy() {
     window.removeEventListener('wheel', this.handleWheel);
     window.removeEventListener('resize', this.handleResize);
-    this.tl.kill();
-    this.tl = null;
-    console.log('Destroy has been called');
+    if (this.tl) {
+      this.tl.kill();
+      this.tl = null;
+    }
+    //console.log('Destroy has been called');
   }
 
   /**
@@ -79,23 +91,29 @@ export default class ScrollingProjects {
    * @memberof ScrollingProjects.prototype
    * @param {WheelEvent} e - Wheel event passed from event handler
    */
-  handleWheel(e) {
+  handleWheel(e: WheelEvent) {
+    const {
+      tl,
+      currentOffset,
+      maxHeight,
+    } = this;
+
     this.currentOffset = this.getYTranslate(this.current);
-    const percentOffset = +((this.currentOffset / this.maxHeight) * 100).toFixed(4);
+    const percentOffset = +((currentOffset / maxHeight) * 100).toFixed(4);
 
     // Will pause timeline if currently running to handle next steps
-    if (this.tl.isActive()) {
-      this.tl.pause();
+    if (tl?.isActive()) {
+      tl.pause();
     }
 
-    if (e.wheelDeltaY < 0) {
+    if (e.deltaY < 0) {
       // scrolling down
-      this.container.style.transform = (this.currentOffset > (this.maxHeight * -0.75))
+      this.container.style.transform = (currentOffset > (maxHeight * -0.75))
         ? `translate(-50%, ${percentOffset - (e.deltaY * 0.01)}%)`
         : 'translate(-50%, -25%)';
     } else {
       // scrolling up
-      this.container.style.transform = (this.currentOffset < (this.maxHeight * -0.25))
+      this.container.style.transform = (currentOffset < (maxHeight * -0.25))
         ? `translate(-50%, ${percentOffset - (e.deltaY * 0.01)}%)`
         : 'translate(-50%, -75%)';
     }
@@ -117,10 +135,10 @@ export default class ScrollingProjects {
       */
       const timelinePosition = (
         this.loopLength * (((((percentOffset - (e.deltaY * 0.01)) * -1) - 25) / 0.5))) / 100;
-      if (e.wheelDeltaY < 0) {
-        this.tl.play(timelinePosition);
+      if (e.deltaY < 0) {
+        tl?.play(timelinePosition);
       } else {
-        this.tl.reverse(timelinePosition);
+        tl?.reverse(timelinePosition);
       }
     }, 300);
   }
@@ -131,12 +149,16 @@ export default class ScrollingProjects {
    * @memberof ScrollingProjects.prototype
    */
   initSliding() {
-    this.tl = new TimelineMax();
+    const {
+      container,
+      loopLength,
+    } = this;
+
+    this.tl = gsap.timeline();
     this.tl.fromTo(
-      this.container,
-      this.loopLength,
+      container,
       { x: '-50%', y: '-25%' },
-      { x: '-50%', y: '-75%', ease: Power0.easeNone },
+      { x: '-50%', y: '-75%', ease: Power0.easeNone, duration: loopLength },
       0,
     );
     this.tl.repeat(5);
@@ -146,10 +168,8 @@ export default class ScrollingProjects {
    * Returns current Y translate of element
    * @function getYTranslate
    * @memberof ScrollingProjects.prototype
-   * @param {CSSStyleDeclaration} el - object containing element's style declarations
-   * @returns {string} - String repesenting current translation in Y axis
    */
-  getYTranslate(el) {
-    return el.getPropertyValue('transform').replace(/[^0-9\-.,]/g, '').split(',')[5];
+  getYTranslate(el: CSSStyleDeclaration): number {
+    return +el.getPropertyValue('transform').replace(/[^0-9\-.,]/g, '').split(',')[5];
   }
 }
