f291d454187ab36a90616dbca9147d7414ddbf72
Luke <lukecochrane2@gmail.com>
2020-09-30 23:27:53 -0400

    Refactoring shaders to string literal

--- src/js/glow-process/glow-shader-y.ts
+++ src/js/glow-process/glow-shader-y.ts
@@ -1,59 +1,55 @@
 const GlowShaderVert = {
 
   uniforms: {
-    "texOne": {
+    texOne: {
       value: null
     },
-    "resolution": {
+    resolution: {
       value: null
     },
   },
 
-  vertexShader: [
+  vertexShader:`
+    varying highp vec2 vUv;
 
-    "varying highp vec2 vUv;",
+    void main() {
 
-    "void main() {",
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 
-    "vUv = uv;",
-    "gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+    }
+  `,
 
-    "}"
+  fragmentShader:`
+    #define PI 3.141592653589793
+    #define E 2.718281828459045
+    uniform sampler2D texOne;
+    uniform vec2 resolution;
 
-  ].join("\n"),
+    varying highp vec2 vUv;
+    float calcGauss(in float x, in float std) {
+      float stdSq = std * std;
+      return (1. / sqrt(2. * PI * stdSq)) * pow(E, -(x * x) / (2. * stdSq));
+    }
 
-  fragmentShader: [
-    "#define PI 3.141592653589793",
-    "#define E 2.718281828459045",
-    "uniform sampler2D texOne;",
-    "uniform vec2 resolution;",
+    void main(){
 
-    "varying highp vec2 vUv;",
-    "float calcGauss(in float x, in float std) {",
-      "float stdSq = std * std;",
-      "return (1. / sqrt(2. * PI * stdSq)) * pow(E, -(x * x) / (2. * stdSq));",
-    "}",
+    vec2 onePixel = vec2(1.0, 1.0) / resolution;
+    vec4 texel0 = texture2D(texOne, vUv);
+    vec4 col = vec4(vec3(0.), 1.);
 
-    "void main(){",
+    float sum = 0.;
 
-    "vec2 onePixel = vec2(1.0, 1.0) / resolution;",
-    "vec4 texel0 = texture2D(texOne, vUv);",
-    "vec4 col = vec4(vec3(0.), 1.);",
-
-    "float sum = 0.;",
-
-    "for (float index = 0.; index < 50.; index++) {",
-    "float offset = (index/50. - 0.5);",
-    "vec2 theUv = vUv + vec2(0., offset * 0.1 * (resolution.y / resolution.x));\
-      float spot = offset - 25.;\
-      float gauss = calcGauss(offset, 0.3);\
-      col += texture2D(texOne, theUv) * gauss;\
-      sum += gauss;\
-    }",
-    "gl_FragColor = vec4(col/sum);",
-    "}"
-
-  ].join("\n")
+    for (float index = 0.; index < 50.; index++) {
+    float offset = (index/50. - 0.5);
+    vec2 theUv = vUv + vec2(0., offset * 0.1 * (resolution.y / resolution.x));
+      float spot = offset - 25.;
+      float gauss = calcGauss(offset, 0.3);
+      col += texture2D(texOne, theUv) * gauss;
+      sum += gauss;
+    }
+    gl_FragColor = vec4(col/sum);
+    }`
 };
 
 export {
